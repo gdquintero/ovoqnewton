d972114655b2bde15781b2fdb190ce7ed15af22a
Gustavo <gustavoquintero9105@gmail.com>
2026-06-14 11:06:04 -0500

    Stop tracking compiled artifacts, add gitignore

--- sources/run_andreani.sh
+++ sources/run_andreani.sh
@@ -1,15 +1,23 @@
+#!/bin/bash
 rm -f salida.txt param.txt
 touch salida.txt param.txt
 
-export ALGENCAN=~/algencan-3.1.1
-rm -f andreani
+export ALGENCAN=$HOME/algencan-3.1.1
+
+rm -f andreani *.mod *.o
+
+# Compilar dependencias con el gfortran de ESTA máquina
+bash sort.sh   || exit 1
+bash subset.sh || exit 1
+
+# Enlazar el principal
 gfortran -O3 -w -fcheck=all -g andreani.f90 -L$ALGENCAN/lib -lalgencan -lhsl sort.o subset.o -o andreani
 
 delta=1.0d-3
 sigmin=1.0d-1
 gamma=5.d0
 noutliers=0
-echo $delta $sigmin $gamma $noutliers  > param.txt
+echo $delta $sigmin $gamma $noutliers > param.txt
 ./andreani
 
 
